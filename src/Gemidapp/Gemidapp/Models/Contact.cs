ccbe584e51cb45f8d287148a7ef42f4e861d1689
Patrick <Patrickhacens@gmail.com>
2017-08-13 22:22:47 -0300

    contact model

---
+++ src/Gemidapp/Gemidapp/Models/Contact.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gemidapp.Models
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+
+        public string Number { get; set; }
+    }
+}
